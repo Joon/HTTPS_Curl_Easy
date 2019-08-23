c4edd373e3df3c7f37693bc09c90ff7899a8487e
Joon <joon.durandt@gmail.com>
2019-08-23 08:50:22 +0100

    Added first pass of code

---
+++ jannson_sphere/jansson_private_config.h
@@ -0,0 +1,28 @@
+/* #undef HAVE_ENDIAN_H */
+#define HAVE_FCNTL_H 1
+/* #undef HAVE_SCHED_H */
+/* #undef HAVE_UNISTD_H */
+/* #undef HAVE_SYS_PARAM_H */
+#undef HAVE_SYS_STAT_H
+/* #undef HAVE_SYS_TIME_H */
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_STDINT_H 1
+
+#define HAVE_CLOSE 1
+#undef HAVE_GETPID 
+/* #undef HAVE_GETTIMEOFDAY */
+#undef HAVE_OPEN 
+#undef HAVE_READ
+/* #undef HAVE_SCHED_YIELD */
+
+/* #undef HAVE_SYNC_BUILTINS */
+/* #undef HAVE_ATOMIC_BUILTINS */
+
+#define HAVE_LOCALE_H 1
+#define HAVE_SETLOCALE 1
+
+#undef USE_URANDOM
+#undef USE_WINDOWS_CRYPTOAPI
+
+#define INITIAL_HASHTABLE_ORDER 3
